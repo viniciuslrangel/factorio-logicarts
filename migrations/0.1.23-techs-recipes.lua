61c8984bdee0090a95a179f6e000fcc24e24ff2b
dorfl <sean.pringle@gmail.com>
2018-07-19 09:15:18 +1000

    track migrations

---
+++ migrations/0.1.23-techs-recipes.lua
@@ -0,0 +1,17 @@
+local check = function(force, techname)
+  if force.technologies[techname] ~= nil and force.technologies[techname].researched then
+    force.technologies[techname].researched = false
+    force.technologies[techname].researched = true
+  end
+end
+
+for i, force in pairs(game.forces) do
+  force.reset_recipes()
+  force.reset_technologies()
+  check(force, "logicarts-tech1")
+  check(force, "logicarts-tech2")
+  check(force, "logicarts-tech-stops")
+  check(force, "logicarts-tech-stickers")
+  check(force, "logicarts-tech-groups")
+  check(force, "logicarts-tech-dual")
+end
